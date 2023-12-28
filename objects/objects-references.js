a4ffa3171a3eb9fef73c7798fd9dd771f5cc38aa
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-12-28 22:20:08 +0530

    added expense manager

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,3 +1,6 @@
+//object ki property ko modify kr skte hai function me ya bahar bhi 
+//agr object ko initialize kr diya to new object create ho jata hai wo same object ko refer nhi krta
+
 let myAccount = {
     name: 'Shivangi',
     expenses: 0,
@@ -8,8 +11,24 @@ let addExpense = function(account, amount){
     account.expenses= account.expenses+ amount
 }
 
-addExpense(myAccount,2.50)
-console.log(myAccount)
+let addIncome = function(account,amount){
+    account.income=  account.income + amount
+}
 
-//object ki property ko modify kr skte hai function me ya bahar bhi 
-//agr object ko initialize kr diya to new object create ho jata hai wo same object ko refer nhi krta
+let resetAccount = function(account){
+    account.expenses=0
+    account.income=0
+}
+
+let getAccountSummary = function(account){
+    let restAmount = account.income - account.expenses
+
+    console.log(`Account for ${account.name} has ${restAmount}. ${account.income} in income. ${account.expenses} in expenses.`)
+}
+
+addIncome(myAccount,1000)
+addExpense(myAccount,100)
+addExpense(myAccount,200)
+getAccountSummary(myAccount)
+resetAccount(myAccount)
+getAccountSummary(myAccount)
