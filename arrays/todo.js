59d70f61808b9830b3db85e9e363d0338241c5fa
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-12-29 17:41:21 +0530

    added arrays

---
+++ arrays/todo.js
@@ -0,0 +1,6 @@
+const todos = ['Do some Yoga' , 'Read the book' , 'Have dryfruits' , 'Do some painting' , 'Do skin care']
+
+console.log(`You have ${todos.length} todos `)
+
+console.log(`TODO: ${todos[0]}`)
+console.log(`TODO: ${todos[todos.length-2]}`)
