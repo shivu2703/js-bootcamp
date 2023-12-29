cf3c0a870b6e62b5564ba9a7457c8cb967798f9b
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-12-29 14:33:53 +0530

    added strings-methods.js

---
+++ objects/strings-methods.js
@@ -0,0 +1,28 @@
+let name = '  Shivangi Katiyar  '
+
+//length property
+console.log(name.length)
+
+//convert to uppercase
+console.log(name.toUpperCase())
+
+//convert to lowercase
+console.log(name.toLowerCase())
+
+let password = 'abc983password'
+
+//includes method
+console.log(password.includes('password'))
+
+//trim method
+console.log(name.trim())
+
+//challenge area
+
+let isValidPassword = function(password){
+    return password.length>8 && (!password.includes('password'))
+}
+
+console.log(isValidPassword('hihi'))
+console.log(isValidPassword('abc2389^*&*(&'))
+console.log(isValidPassword('hikspassword'))
