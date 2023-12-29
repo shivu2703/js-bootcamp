872f8ad166cafd4480519aac7b6a48850f71a3d7
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-12-29 23:02:22 +0530

    added for loop

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,15 +1,24 @@
 const notes =[ 'Note 1', 'Note 2', 'Note 3']
 
 // pop and push - delete and add element in the end
-notes.push('Note 4')
-notes.pop()
+// notes.push('Note 4')
+// notes.pop()
 
 //shift and unshift - delete and add element in the start of array
-notes.shift()
-notes.unshift('Note 1')
+// notes.shift()
+// notes.unshift('Note 1')
 
 //splice- we use add and remove elemet at the same time 
 // (index from where you want to delete , how many element you want to delete, element you want to add at the given index)
-notes.splice(2,0,'hi')
+// notes.splice(2,0,'hi')
+
+notes.forEach(function(note,index){
+  console.log(index)
+  console.log(note)
+})
 console.log(`We have ${notes.length} notes`)
 console.log(notes)
+
+for(let count =0;count<notes.length;count++){
+    console.log(notes[count])
+}
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,11 +1,19 @@
 const todos = ['Do some Yoga' , 'Read the book' , 'Have dryfruits' , 'Do some painting' , 'Do skin care']
 
 //challenge
-todos.splice(2,1)
-todos.push('have your breaksfast on time')
-todos.shift()
-console.log(`You have ${todos.length} todos`)
-console.log(todos)
+// todos.splice(2,1)
+// todos.push('have your breaksfast on time')
+// todos.shift()
 
+todos.forEach(function(todo,index){
+    const num = index + 1
+    console.log(`${num}. ${todo}`)
+})
 
+// console.log(`You have ${todos.length} todos`)
+// console.log(todos)
+
+for ( let count =0 ;count<todos.length;count++){
+    console.log(`${count +1}. ${todos[count]}`)
+}
 
