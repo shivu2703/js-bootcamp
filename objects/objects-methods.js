2f8c9b61a47e5b0d61f024c50a7f70294cf87888
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-12-29 14:13:43 +0530

    added methods

---
+++ objects/objects-methods.js
@@ -0,0 +1,24 @@
+// method is nothing but as a object property whose value is a function
+
+let restuarant = {
+    name: 'chai sqaure',
+    guestCapacity: 75,
+    guestCount: 0,
+    checkAvailability: function(partysize){
+        let leftSeat = this.guestCapacity - this.guestCount
+        return leftSeat >= partysize
+    },
+    seatParty: function(partysize){
+        this.guestCount= this.guestCount+ partysize
+    },
+    removeParty: function(partysize){
+        this.guestCount= this.guestCount - partysize
+    }
+
+
+}
+
+restuarant.seatParty(72)
+console.log(restuarant.checkAvailability(4))
+restuarant.removeParty(5)
+console.log(restuarant.checkAvailability(4))
