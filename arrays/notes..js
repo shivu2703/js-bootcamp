9e8c825f986940266909f0793b9df8d5367d7a2b
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2023-12-29 20:19:04 +0530

    added array methods

--- arrays/notes..js
+++ arrays/notes..js
@@ -1,6 +1,15 @@
 const notes =[ 'Note 1', 'Note 2', 'Note 3']
 
-console.log(notes.length)
+// pop and push - delete and add element in the end
+notes.push('Note 4')
+notes.pop()
 
-console.log(notes[0])
-console.log(notes[notes.length-2])
+//shift and unshift - delete and add element in the start of array
+notes.shift()
+notes.unshift('Note 1')
+
+//splice- we use add and remove elemet at the same time 
+// (index from where you want to delete , how many element you want to delete, element you want to add at the given index)
+notes.splice(2,0,'hi')
+console.log(`We have ${notes.length} notes`)
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,6 +1,11 @@
 const todos = ['Do some Yoga' , 'Read the book' , 'Have dryfruits' , 'Do some painting' , 'Do skin care']
 
-console.log(`You have ${todos.length} todos `)
+//challenge
+todos.splice(2,1)
+todos.push('have your breaksfast on time')
+todos.shift()
+console.log(`You have ${todos.length} todos`)
+console.log(todos)
 
-console.log(`TODO: ${todos[0]}`)
-console.log(`TODO: ${todos[todos.length-2]}`)
+
+
