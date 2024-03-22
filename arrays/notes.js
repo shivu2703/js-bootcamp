bf57d3ee3461dec9fe30c30b23991e70c9f40949
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-03-22 23:05:59 +0530

    added findNote method

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,18 +1,27 @@
-const notes=[{},{
-  title:'Next trip',
-  body:'Kasol'
+const notes=[{
+    title:'Next trip',
+    body:'Kasol'
 },{
-  title:'habbits to work on',
-  body:'Exercise'
+    title:'habbits to work on',
+    body:'Exercise'
 },{
-  title:'office modification',
-  body:'get a new seat'
+    title:'office modification',
+    body:'get a new seat'
 }]
 
-const index= notes.findIndex(function(note,index){
-    console.log(note)
-    return note.title === 'habbits to work on'
-})
+const findNote=function(notes,noteTitle){
+    const index=notes.findIndex(function(note,index){
+      return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+  return notes[index]
+}
 
-console.log(index)
+const note= findNote(notes,'Habbits to work on')
+console.log(note)
+// const index= notes.findIndex(function(note,index){
+//     console.log(note)
+//     return note.title === 'habbits to work on'
+// })
+
+// console.log(index)
 
