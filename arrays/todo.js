70ff7c295d87c894cec3ebc790050bad67449c9d
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-03-22 23:26:25 +0530

    added deleteTodos method

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,23 +1,30 @@
-const todos = ['Do some Yoga' , 'Read the book' , 'Have dryfruits' , 'Do some painting' , 'Do skin care']
+const todos = [{
+    text:'Do some Yoga',
+    completed: true
+}, {
+    text:'Read the book',
+    completed:false
+}, {
+    text:'Have dryfruits',
+    completed:true 
+},{ 
+    text:'Do some painting',
+    completed:false
+},{ 
+    text:'Do skin care',
+    completed:true
+}]
 
-//challenge
-// todos.splice(2,1)
-// todos.push('have your breaksfast on time')
-// todos.shift()
 
-todos.forEach(function(todo,index){
-    const num = index + 1
-    console.log(`${num}. ${todo}`)
-})
+const deleteTodo = function(todos,todoText){
+      const index=todos.findIndex(function(todo,index){
+            return todo.text.toLowerCase() === todoText.toLowerCase()
+      })
+      
+      if(index > -1){
+        todos.splice(index,1)
+      }
+}
 
-// console.log(`You have ${todos.length} todos`)
-// console.log(todos)  
-
-// for(let i=0;i<3;i++){
-//     console.log(i)
-// }
-
-// for ( let count =0 ;count<todos.length;count++){
-//     console.log(`${count +1}. ${todos[count]}`)
-// }
-
+deleteTodo(todos,'do skin care')
+console.log(todos)
