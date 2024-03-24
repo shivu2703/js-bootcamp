87aeee4fd485e131b15446b144c66e7d89f4e65c
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-03-25 00:24:17 +0530

    added few more methods of dom

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,2 +1,13 @@
-const p=document.querySelector('h1')
-p.remove()
+//DOM- Document Object Model
+
+// Query and Remove
+// const p=document.querySelector('h1')
+// p.remove()
+
+// Query All and remove
+const ps=document.querySelectorAll('p')
+ps.forEach(function(p){
+    //p.remove()
+    // console.log(p.textContent)
+    p.textContent="**************"
+})
