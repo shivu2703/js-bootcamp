501fc9a9e2c4817bb5c7a8400ee329c5ced94a4e
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-03-25 02:01:35 +0530

    added practice folder

---
+++ practice-files/practice-25-3.js
@@ -0,0 +1,24 @@
+//DOM- Document Object Model
+
+// Query and Remove
+// const p=document.querySelector('h1')
+// p.remove()
+
+// Query All and remove
+const ps=document.querySelectorAll('p')
+ps.forEach(function(p){
+    //p.remove()
+    // console.log(p.textContent)
+    p.textContent="**************"
+})
+
+//Add a new element 
+
+//create a new element
+const newParagraph= document.createElement('p')
+
+// give some meaning content to this above created element
+newParagraph.textContent='This is new element from javascript.'
+
+//place that element in html file
+document.querySelector('body').appendChild(newParagraph)
