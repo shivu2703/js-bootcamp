764dd2b211e654905207ef8c5f701958b1188565
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-03-25 02:00:51 +0530

    added button clicked functionality

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -8,27 +8,8 @@ const notes=[{
     title:'office modification',
     body:'get a new seat'
 }]
-//DOM- Document Object Model
 
-// Query and Remove
-// const p=document.querySelector('h1')
-// p.remove()
+document.querySelector('button').addEventListener('click',function(e){
+    e.target.textContent='The button was clicked'
 
-// Query All and remove
-const ps=document.querySelectorAll('p')
-ps.forEach(function(p){
-    //p.remove()
-    // console.log(p.textContent)
-    p.textContent="**************"
 })
-
-//Add a new element 
-
-//create a new element
-const newParagraph= document.createElement('p')
-
-// give some meaning content to this above created element
-newParagraph.textContent='This is new element from javascript.'
-
-//place that element in html file
-document.querySelector('body').appendChild(newParagraph)
