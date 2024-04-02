3794588c5c68df436dd2d7981edd08bce606d5bf
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-04-02 13:07:31 +0530

    added input event

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -19,9 +19,12 @@ document.querySelector('#remove-all').addEventListener('click',function(e){
     })
 })
 
-document.querySelector('#search-text').addEventListener('change',function(e){
+document.querySelector('#search-text').addEventListener('input',function(e){
     console.log(e.target.value)
 })
+// document.querySelector('#search-text').addEventListener('change',function(e){
+//     console.log(e.target.value)
+// })
 
 
 // document.querySelector('button').addEventListener('click',function(e){
