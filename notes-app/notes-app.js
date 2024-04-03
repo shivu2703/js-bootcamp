4eb479923c4babc78eb74eb200beb3bdba97d1b5
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-04-04 01:24:38 +0530

    added refactoring in notes-app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,48 +1,25 @@
-const notes=getSavedNotes()
+const notes = getSavedNotes()
 
-const filters={
+const filters = {
     searchText: ''
 }
 
-const renderNotes = function(notes,filters){
-    const filteredNotes = notes.filter(function(note){
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
+renderNotes(notes, filters)
 
-    console.log(filteredNotes)
-    
-    document.querySelector('#notes').innerHTML= ''
-
-    filteredNotes.forEach(function(note){
-        const noteEL= document.createElement('p')
-        if(note.title.length >0){
-            noteEL.textContent= note.title
-        }else{
-            noteEL.textContent='Unnamed notes'
-        }
-       
-        document.querySelector('#notes').appendChild(noteEL)
-    })
-}
-
-// renderNotes(notes,filters)
-
-document.querySelector('#search-text').addEventListener('input',function(e){
-    filters.searchText=e.target.value
-    renderNotes(notes,filters)
-})
-
-document.querySelector('#create-note').addEventListener('click',function(e){
+document.querySelector('#create-note').addEventListener('click', function (e) {
     notes.push({
-        title:'',
-        body:''
+        title: '',
+        body: ''
     })
-    localStorage.setItem('notes',JSON.stringify(notes))
-    renderNotes(notes,filters)
+    saveNotes(notes)
+    renderNotes(notes, filters)
 })
 
-document.querySelector('#filter-by').addEventListener('change',function(e){
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
     console.log(e.target.value)
 })
-
-
