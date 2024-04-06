b7a5f483860cc4062472624382dbcf0382f7d698
Shivangi Katiyar <2016247@iiitdmj.ac.in>
2024-04-06 23:56:23 +0530

    added new changes

--- todo-app/todo-function.js
+++ todo-app/todo-function.js
@@ -22,7 +22,6 @@ const renderTodos = function (todos, filters) {
         return searchTextMatch && hideCompletedMatch
     })
 
-
     // debugger
     const incompleteTodos = filteredTodos.filter(function (todo) {
         return !todo.completed
@@ -55,7 +54,7 @@ const generateTodoDOM = function (todo) {
     button.textContent='x'
     todoEL.appendChild(button)
 
-     return todoEL
+    return todoEL
 }
 
 // Get the DOM elements for list summary
